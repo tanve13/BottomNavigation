461154f747257398c39bc945ab7c9eef3233defe
tanve13 <tanusaini.jandir@gmail.com>
2024-06-27 08:48:40 +0530

    backspace_update

--- app/src/main/java/com/tanveer/bottomnavigationapplication/MainActivity.kt
+++ app/src/main/java/com/tanveer/bottomnavigationapplication/MainActivity.kt
@@ -1,4 +1,4 @@
- package com.tanveer.bottomnavigationapplication
+package com.tanveer.bottomnavigationapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,22 +6,41 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.tanveer.bottomnavigationapplication.databinding.ActivityMainBinding
 
- class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
-     var navController: NavController? = null
+    var navController: NavController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        binding?.bottomNav?.setOnItemSelectedListener{
-            when(it.itemId){
-                R.id.home ->  navController?.navigate(R.id.firstFragment)
-                R.id.details ->  navController?.navigate(R.id.secondFragment)
-                R.id.login ->  navController?.navigate(R.id.thirdFragment)
+        binding?.bottomNav?.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> navController?.navigate(R.id.firstFragment)
+                R.id.details -> navController?.navigate(R.id.secondFragment)
+                R.id.login -> navController?.navigate(R.id.thirdFragment)
             }
             return@setOnItemSelectedListener true
         }
         navController = findNavController(R.id.host)
+
+        navController?.addOnDestinationChangedListener { navController, destination, arguments ->
+            when (destination.id) {
+                R.id.firstFragment -> {
+                    supportActionBar?.title = resources.getString(R.string.first_fragment)
+                    binding?.bottomNav?.menu?.findItem(R.id.home)?.isChecked = true
+                }
+
+                R.id.secondFragment -> {
+                    supportActionBar?.title = resources.getString(R.string.second_fragment)
+                    binding?.bottomNav?.menu?.findItem(R.id.details)?.isChecked = true
+                }
+
+                R.id.thirdFragment -> {
+                    supportActionBar?.title = resources.getString(R.string.third_fragment)
+                    binding?.bottomNav?.menu?.findItem(R.id.login)?.isChecked = true
+                }
+            }
+        }
     }
 
 }
